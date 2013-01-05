7f04cf8abfff4ce0f584c7645cd76b89351ecf23
Pindi Albert <pindi.albert@gmail.com>
2013-01-04 22:41:44 -0800

    Install South

--- antonym/settings.py
+++ antonym/settings.py
@@ -126,6 +126,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     'antonym',
+    'south',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
